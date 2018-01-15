c9b8f0551a8fa72c4c2524fbe7d90c0b6caab320
Christopher D. McMurrough <cmcmurrough@users.noreply.github.com>
2018-01-14 23:05:13 -0600

    Update networkhandler.cc

--- examples/WINDOWS/networkhandler.cc
+++ examples/WINDOWS/networkhandler.cc
@@ -23,8 +23,6 @@
 // values needed from the connection manager
 extern CipConn* g_active_connection_list;
 
-#define DEBUG true
-
 /** @brief The number of bytes used for the Ethernet message buffer on
  * the PC port. For different platforms it may make sense to
  * have more than one buffer.
@@ -489,7 +487,7 @@ EipStatus SendUdpData( struct sockaddr_in* address, int socket, BufReader aOutpu
 }
 
 
-#if defined(DEBUG)
+//#if defined(DEBUG)
 static void dump( const char* aPrompt, EipByte* aBytes, int aCount )
 {
     int len = printf( "%s:", aPrompt );
@@ -504,7 +502,7 @@ static void dump( const char* aPrompt, EipByte* aBytes, int aCount )
 
     printf( "\n" );
 }
-#endif
+//#endif
 
 
 // This can block on the second time through the loop, so may not be a good
@@ -591,9 +589,9 @@ EipStatus HandleDataOnTcpSocket( int socket )
                 return kEipStatusError;
             }
 
-#if defined(DEBUG)
+//#if defined(DEBUG)
             dump( "bigTCP", s_packet, num_read );
-#endif
+//#endif
 
             if( packetz != 0 && packetz < sizeof(s_packet) )
             {
@@ -624,9 +622,9 @@ EipStatus HandleDataOnTcpSocket( int socket )
         // we got the right amount of data
         packetz += 4;
 
-#if defined(DEBUG)
+//#if defined(DEBUG)
         dump( "rTCP", s_packet, num_read + 4 );
-#endif
+//#endif
 
         // TODO handle partial packets
         CIPSTER_TRACE_INFO( "Data received on tcp:\n" );
